41a9bcb9502270ea8171074fe14c74de755aa3be
Benjamin Hoover <benjamin.hoover@ibm.com>
2020-10-14 17:05:26 -0400

    Clean repository from old implementation

--- backend/path_fixes.py
+++ backend/path_fixes.py
@@ -11,9 +11,3 @@ PUBLIC = CLIENT / "public"
 DIST = PUBLIC
 DATA = ROOT / "data"
 PROJECT = ROOT / "biohash_project.yaml"
-
-# REF = DATA / "annotations"
-# H5_CORPUS = REF / "leo_correlations_100k_codes_uint16.hdf5"
-# H5_CORPUS = REF / "leo_info_coef_50k_50t.hdf5"
-# ENCS = REF / "encodings.npy"
-# OFFS = REF / "offsets_0.npy"
--- backend/server/main.py
+++ backend/server/main.py
@@ -1,7 +1,6 @@
 import argparse
 from typing import *
 import numpy as np
-from datetime import datetime
 
 from fastapi import FastAPI
 from fastapi.responses import FileResponse, RedirectResponse
@@ -10,15 +9,9 @@ from fastapi.staticfiles import StaticFiles
 import uvicorn
 import server.api as api
 import path_fixes as pf
-from server.utils import round_2d_nested_list, round_1d_list
 from biohasher import get_project
-from indexer import BiohashIndexer
 
 from cachetools import cached
-
-@cached(cache={})
-def get_indexer(h5_fname=pf.H5_CORPUS, enc_file=pf.ENCS, off_file=pf.OFFS):
-    return BiohashIndexer(h5_fname, enc_file, off_file)
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--port", default=8000, type=int, help="Port to run the app. ")
@@ -58,70 +51,11 @@ async def sentence_to_tokens(sentence: str):
     tokens = project.tokenizer.tokenize(sentence)
     return tokens
 
-@app.post("/api/attentions-from-tokens")
-async def attentions_from_tokens(payload: api.AttentionFromTokenPayload):
-    tokens = payload['tokens']
-    target_idx = payload['target_idx']
-    hash_length = payload['hash_length']
-
-    project = get_project(pf.PROJECT)
-    token_ids = project.tokenizer.tokens2ids(tokens)
-    output = project.get_hash_from_token_ids(token_ids, target_idx, hash_length)
-
-    return {
-        "tokens": tokens,
-        "token_ids": token_ids,
-        "target_idx": target_idx,
-        "context_attentions": round_2d_nested_list(output['context_attentions'].tolist(), 6),
-        "target_attentions": round_1d_list(output['target_attentions'].tolist(), 6) if target_idx is not None else None,
-        "activated_neurons": output['activated_neurons'].tolist(),
-        "hashcode": output['hash'].tolist()
-    }
-
 @app.get("/api/get-memory-concepts")
 def get_memory_concepts(head_index: int, n_show:int=20, beta:float=800):
     """Fetch the target concepts for a particular memory"""
     project = get_project(pf.PROJECT)
     return project.get_mem_concepts(head_index, n_show=n_show, beta=beta)
-
-# @app.post("/api/get-k-nearest-context-independent")
-# async def get_k_nearest_context_independent(payload: api.KNearestPayload):
-#     hash_query = payload["hash_query"]
-#     k = payload["k"]
-#     hash_length = payload["hash_length"]
-
-#     project = get_project(pf.PROJECT)
-#     k_nearest_tokens = project.get_k_neighbors(hash_query, k, hash_length)
-
-#     return {
-#         "tokens": k_nearest_tokens,
-#     }
-
-# @app.post("/api/get-k-nearest-context-dependent")
-# async def get_k_nearest_context_dependent(payload: api.KNearestPayload):
-#     hash_query = payload["hash_query"]
-#     k = payload["k"]
-#     hash_length = payload["hash_length"]
-
-#     indexer = get_indexer()
-
-#     project = get_project(pf.PROJECT)
-
-#     start = datetime.now()
-#     res = indexer.get_k_nearest(np.array(hash_query), k)
-#     print(f"Indexing took {datetime.now() - start} seconds")
-
-#     def parse_nearest(sim, toks, wgram_ind, target_ind, wgram):
-#         return {
-#             "sentence_tokens": project.tokenizer.ids2tokens(toks),
-#             "sim_score": sim,
-#             "wgram_start": wgram_ind,
-#             "wgram": project.tokenizer.ids2tokens(wgram),
-#             "target_start": target_ind,
-#         }
-
-#     json_output = [parse_nearest(sim, toks, wg, ti, gram) for sim, toks, wg, ti, gram in zip(res['sims'], res['sentence_tokens'], res['wgram_inds'], res['target_inds'], res['wgrams'])]
-#     return json_output
 
 if __name__ == "__main__":
     # This file is not run as __main__ in the uvicorn environment
