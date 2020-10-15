efdb1a88641f3c75d8d267563d5179e3455f3a67
Benjamin Hoover <benjamin.hoover@ibm.com>
2020-10-14 23:00:32 -0400

    Create word cloud

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,23 +1,22 @@
 <script lang="ts">
 	import type {Concept} from "./types"
+	import WordCloud from "./components/WordCloud.svelte"
 	import {API} from "./api"
-
-	export let name: string;
+	import * as _ from "lodash"
 
 	const api = new API()
 
-	let headIndex: number = 1;
+	let headIndex: number = undefined;
 	let conceptList: Concept[] | null = null
 
-	function clicky() {
-		api.getMemoryConcepts(headIndex).then(r => {
+	function inputty(e) {
+		let mem = +e.target.value;
+
+		// TODO: Debounce this api call
+		api.getMemoryConcepts(mem).then(r => {
+			headIndex = mem
 			conceptList = r
 		})
-	}
-
-	function inputty(e) {
-		headIndex = +e.target.value;
-		console.log("INPUTTED: ", headIndex);
 	}
 </script>
 
@@ -44,18 +43,15 @@
 </style>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>
-		Visit the
-		<a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-		to learn how to build Svelte apps.
-	</p>
-	<div><input type="range" min="0" max="399" on:input={inputty} /></div>
-	<button on:click={clicky}>What concepts for memory {headIndex}?</button>
+	<h1>FlyBrain Explorer</h1>
+	{#if headIndex != undefined}
+		<h3>Showing Concepts for Head {headIndex + 1}</h3>
+	{:else}
+		<h5>Move the slider to show concepts!</h5>
+	{/if}
+	<div><input type="range" min="0" max="399" on:input={_.debounce(inputty, 150)} /></div>
 
 	{#if conceptList != null}
-		<pre>
-			{JSON.stringify(conceptList)}
-		</pre>
+		<WordCloud concepts={conceptList}></WordCloud>
 	{/if}
 </main>
---
+++ client/src/components/WordCloud.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+    import * as d3 from "d3";
+    import cloud from "d3-cloud";
+    import type { Word } from "d3-cloud";
+    import type * as tp from "../types";
+
+    export let concepts: tp.Concept[] = [];
+    let isMounted: boolean = false;
+    $: console.log("MY CONCEPTS: ", concepts);
+    $: contributions = concepts.map((d) => d.contribution);
+
+    $: sizeScale = d3
+        //@ts-ignore
+        .scaleLinear()
+        .domain([0, d3.max(contributions)])
+        .range([10, 100]);
+
+    $: words = concepts.map((c) => {
+        return {
+            text: c.token,
+            size: sizeScale(c.contribution),
+        } as Word;
+    });
+
+    let displayWords: Word[] | null = null;
+
+    function redefineWords(newWords: Word[]) {
+        console.log("OG words: ", words);
+        console.log("New words: ", newWords);
+        displayWords = newWords;
+    }
+
+    $: layout = cloud()
+        .size([500, 500])
+        .words(words)
+        .padding(5)
+        .rotate(function () {
+            return ~~(Math.random() * 2) * 90;
+        })
+        .font("Impact")
+        .fontSize(function (d) {
+            console.log("RUNNING D: ", d);
+            return d.size;
+        })
+        .on("end", redefineWords)
+        .start();
+</script>
+
+<div class="cloud">
+    {#if displayWords != null}
+        <svg width={layout.size()[0]} height={layout.size()[1]}>
+            <g
+                transform={`translate(${layout.size()[0] / 2},${layout.size()[1] / 2})`}>
+                {#each displayWords as word (word.text)}
+                    <text
+                        font-size={word.size + 'px'}
+                        font-family={word.font}
+                        text-anchor="middle"
+                        transform={`translate(${word.x}, ${word.y})rotate(${word.rotate})`}>
+                        {word.text}
+                    </text>
+                {/each}
+            </g>
+        </svg>
+    {/if}
+</div>
